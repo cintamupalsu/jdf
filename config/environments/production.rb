02bb824caecc0335ba8e087b9d7bfaa775697518
a_maulana <cintamupalsu@hotmail.com>
2022-02-11 14:45:25 +0900

    sg2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   host = 'japandf.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default:charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {

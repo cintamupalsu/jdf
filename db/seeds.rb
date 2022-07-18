742ffea6a040eb8e6f054e8e1a308375c175fcc0
a_maulana <cintamupalsu@hotmail.com>
2022-07-18 10:29:30 +0900

    layout unfinish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email: "admin@jdf.com", password: "adminjdf", name: "Admin", developer: 1)
 User.create(email: "editor@jdf.com", password: "editorjdf", name: "Editor", developer: 0)
-UserRole.create(content: "Can manage user")
+UserRole.create(content: "Can manage users")
 UserRole.create(content: "Can manage news")
+UserRole.create(content: "Can manage events")
 UserRoleTransaction.create(user_id: 1, user_role_id: 1)  #Set first user as Admin
 UserRoleTransaction.create(user_id: 1, user_role_id: 2)  #Set first user as Editor
 UserRoleTransaction.create(user_id: 2, user_role_id: 2)  #Set second user as Editor
